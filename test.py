268eb872c7823e7131dd7df2dc7dd83f457a92f6
Ethane1755 <87981364+Ethane1755@users.noreply.github.com>
2022-06-25 13:48:18 +0800

    ver2

--- test.py
+++
@@ -1,6 +0,0 @@
-import requests
-response = requests.get('https://apod.nasa.gov/apod/image/2206/20220624_ALLINEAMENTO_SPECIALEweb.jpg')
-if response.status_code == 200:
-    print('Web site exists')
-else:
-    print('Web site does not exist') 
