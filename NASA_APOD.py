9659f4a1bbdb2d63c2c2b0ea24898ba83e415b88
Ethane1755 <87981364+Ethane1755@users.noreply.github.com>
2022-06-25 14:35:54 +0800

    text

--- NASA_APOD.py
+++ NASA_APOD.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import os
 from datetime import date
+import re
 
 today=date.today()
 d=today.strftime('%y%m%d')
@@ -12,6 +13,28 @@ html = requests.get(url)
 
 s = BeautifulSoup(html.text, 'lxml')
 
+if not os.path.exists("text"):
+    os.mkdir("text")
+txt=s.get_text()
+l=txt.split('astronomer')
+txt=l[1]
+l1=txt.split('Tomorrow')
+print(l1[0])
+exp=str(l1[0])
+l2=exp.split('.')
+exp=('.'.join(l2[1:]))
+exp=exp.replace("\n"," ")
+line=exp.find('Image')
+exp=exp[:line]+'\n'+exp[line:]
+line=exp.find('Explanation')
+exp=exp[:line]+'\n'+exp[line:]
+list1=exp.split('.  ')
+exp=('.\n'.join(list1))
+exp=re.sub(' +', ' ', exp)
+path = "text\\"+d+'.txt'
+with open(path, 'w') as f:
+    f.write(exp)
+
 results=s.find_all("img")
 image_links = [result.get("src") for result in results]
 
@@ -19,18 +42,14 @@ for index, link in enumerate(image_links):
     if not os.path.exists("images"):
         os.mkdir("images")
     link=link[:-5]
-    print(link)
     a=1
     while a!=0:
         response = requests.get(RootUrl+link+'.jpg')
         if response.status_code == 200:
-            print('Web site exists')
             a=0
         else:
-            print('Web site does not exist')
             link=link[:-1]
             a=1
-            print(link)
             print(RootUrl+link+'.jpg')
             k=RootUrl+link+'.jpg'
     img = requests.get(k)
